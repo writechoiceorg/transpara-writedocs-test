b3d8d31f6c697f821df1d854db71e70d26b55e84
Heitor <heitor.j.tessaro@gmail.com>
2023-10-17 16:18:38 -0300

    feat: create FilterComponent and StylizedImage components

---
+++ visual-kpi-docs/src/components/StylizedImage/StylizedImage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+export default function StylizedImage({ imgURL, wSize, alt }) {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <img
+        src={useBaseUrl(imgURL)}
+        alt={alt}
+        style={{
+          width: wSize,
+          margin: '0 0 var(--ifm-paragraph-margin-bottom)',
+          borderRadius: '14px', 
+          boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.2)', 
+        }}
+      />
+    </div>
+  );
+}
