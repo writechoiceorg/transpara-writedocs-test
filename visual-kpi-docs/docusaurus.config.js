6899e18410bd11bc9ed7bcf4cf55a64cf0c16e3e
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-10-30 15:36:59 -0300

    style: sidebar section titles color and weigth

--- visual-kpi-docs/docusaurus.config.js
+++ visual-kpi-docs/docusaurus.config.js
@@ -138,7 +138,7 @@ const config = {
               },
               {
                 label: 'Documentation',
-                to: '/docs/',
+                to: '/docs',
               },
               {
                 label: 'FAQ',
--- visual-kpi-docs/sidebars.js
+++ visual-kpi-docs/sidebars.js
@@ -340,7 +340,6 @@ const sidebars = {
     },
     
   ],
-   
 };
 
 module.exports = sidebars;
