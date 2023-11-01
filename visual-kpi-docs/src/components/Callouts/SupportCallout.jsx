38ed52dff846e2a1e3839dec52eb4dfad74b3127
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-11-01 19:08:13 -0300

    fix: icons size

--- visual-kpi-docs/src/components/Callouts/SupportCallout.jsx
+++ visual-kpi-docs/src/components/Callouts/SupportCallout.jsx
@@ -8,8 +8,8 @@ export default function SupportCallout() {
   return (
     <div className="callout support-callout theme-admonition theme-admonition-note alert alert--secondary admonition_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
       <div className="callout-heading admonitionHeading_node_modules-\@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
-        <span className=".callout-icon admonitionIcon_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
-          <SvgFont data={selection.icons[108]} position={positions["108"]} size={18}/>
+        <span className="callout-icon admonitionIcon_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
+          <SvgFont data={selection.icons[108]} position={positions["108"]} size={22}/>
         </span>
           SUPPORT
       </div>
