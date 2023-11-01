dff27cdf56b36c37def21a82ca5fab062a1d362b
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-11-01 19:01:50 -0300

    fix: margins missing

--- visual-kpi-docs/src/components/Callouts/TrainingCallout.jsx
+++ visual-kpi-docs/src/components/Callouts/TrainingCallout.jsx
@@ -8,8 +8,8 @@ export default function TrainingCallout({ title, content }) {
   return (
     <div className="callout training-callout theme-admonition theme-admonition-note alert alert--secondary admonition_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
       <div className="callout-heading admonitionHeading_node_modules-\@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
-        <span className="admonitionIcon_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
-        <SvgFont data={selection.icons[116]} position={positions["116"]} size={18}/>
+        <span className=".callout-icon admonitionIcon_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
+          <SvgFont data={selection.icons[116]} position={positions["116"]} size={18}/>
         </span>
         {title.toUpperCase()}
       </div>
