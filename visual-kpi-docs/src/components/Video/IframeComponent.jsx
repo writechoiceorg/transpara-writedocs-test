b2847dcf822e225f877d49290b9dcf8ea74d431d
Gabriel Gonçalves <gabrielraeder@outlook.com.br>
2024-02-21 17:39:01 -0300

    feat: adds height prop to iframeComponent

--- visual-kpi-docs/src/components/Video/IframeComponent.jsx
+++ visual-kpi-docs/src/components/Video/IframeComponent.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const IframeComponent = ({ src, wSize }) => {
+const IframeComponent = ({ src, wSize, hSize }) => {
   const iframeStyles = {
     width: wSize || '100%',
+    height: hSize || '400px',
     margin: '0 0 var(--ifm-paragraph-margin-bottom)',
     borderRadius: '7px',
     boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.2)',
   };
 
   return (
-    <>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <iframe
         src={src}
         style={iframeStyles}
-        height="400"
         allowFullScreen
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       />
-    </>
+    </div>
   );
 };
 
