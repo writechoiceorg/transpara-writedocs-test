6ff7038f997a3bc5d923dc7783bb164e9457c03f
Gabriel Gonçalves <gabrielraeder@outlook.com.br>
2024-03-21 19:12:17 -0300

    chore: edits sidebar

---
+++ visual-kpi-docs/.sidebar/supporting-pages/index.js
@@ -0,0 +1,20 @@
+const faqs = require('./faqs');
+
+module.exports = [
+  faqs,
+  {
+    type: 'doc',
+    id: 'supporting-pages/troubleshooting',
+    label: 'Troubleshooting',
+  },
+  {
+    type: 'doc',
+    id: 'supporting-pages/training',
+    label: 'Training',
+  },
+  {
+    type: 'doc',
+    id: 'supporting-pages/glossary',
+    label: 'Glossary',
+  }
+];
