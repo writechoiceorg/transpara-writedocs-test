b3f61447fbbd7421003933f01f3fd95d59587d9e
Gabriel Gonçalves <gabrielraeder@outlook.com.br>
2024-03-25 12:04:34 -0300

    chore: creates pdf faqs pages

---
+++ visual-kpi-docs/.sidebar/supporting-pages/faqs-pdf.js
@@ -0,0 +1,16 @@
+module.exports = {
+  type: 'category',
+  label: 'PDF FAQs',
+  items: [
+    'supporting-pages/faqs-pdf/technical',
+    'supporting-pages/faqs-pdf/implementation',
+    'supporting-pages/faqs-pdf/integrations',
+    'supporting-pages/faqs-pdf/cost-roi',
+    'supporting-pages/faqs-pdf/competition',
+    'supporting-pages/faqs-pdf/use-cases-customers',
+    'supporting-pages/faqs-pdf/using-the-software',
+    'supporting-pages/faqs-pdf/quickstart-pilot',
+    'supporting-pages/faqs-pdf/support-services',
+    'supporting-pages/faqs-pdf/company-background-history-expertise'
+  ],
+}
--- visual-kpi-docs/.sidebar/supporting-pages/index.js
+++ visual-kpi-docs/.sidebar/supporting-pages/index.js
@@ -1,7 +1,9 @@
 const faqs = require('./faqs');
+const faqsPdf = require('./faqs-pdf');
 
 module.exports = [
   faqs,
+  faqsPdf,
   {
     type: 'doc',
     id: 'supporting-pages/troubleshooting',
