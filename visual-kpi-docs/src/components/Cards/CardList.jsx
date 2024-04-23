74b2d80d84e4ea52c78fde30874d713344311e28
Gabriel Gonçalves <gabrielraeder@outlook.com.br>
2024-04-23 15:10:44 -0300

    fix: responsiveness of cards

--- visual-kpi-docs/src/components/Cards/CardList.jsx
+++ visual-kpi-docs/src/components/Cards/CardList.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const CardList = ({ data, Card, className, numColumns }) => {
 
-  const setGridColumns = (numColumns) => {
-      if (numColumns) {
-        const columnWidths = Array(numColumns).fill('minmax(200px, 1fr)').join(' ');
-        return columnWidths;
-      }
-      return 'repeat(auto-fill, minmax(200px, 1fr))';
+  const setGridColumns = () => {
+    if (numColumns) {
+      return `${className} grid-columns-${numColumns}`;
+    } else {
+      return `${className} grid-columns-auto`;
+    }
   };
   
   return (
-    <div className={className} style={{ gridTemplateColumns: setGridColumns(numColumns)}}>
+    <div className={setGridColumns()} >
       {data.map((item, index) => (
         <Card key={index} logo={item.logo} name={item.name} linkTo={item.linkTo} externalLink={item.externalLink} />
       ))}
