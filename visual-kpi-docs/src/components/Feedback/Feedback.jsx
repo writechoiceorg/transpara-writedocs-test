408a4170354a44877f512afbd1a321382ad2ef09
Heitor José Tessaro <heitor@writechoice.com.br>
2024-05-03 10:43:56 -0300

    feat: add emailjs library to use in the Feedback button
    The feedback system is working. Aadd css (not finished). Add button to glossary to test.

--- visual-kpi-docs/src/components/Feedback/Feedback.jsx
+++ visual-kpi-docs/src/components/Feedback/Feedback.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 import styles from './style.module.css';
 
 export default function Feedback() {
@@ -6,25 +7,34 @@ export default function Feedback() {
 
     const toggleDialog = () => setIsOpen(!isOpen);
 
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_mgdklrf', 'template_8tfc8zu', e.target, '1a6aHxEAzXmMBSep5')
+            .then((result) => {
+                console.log('Email successfully sent!', result.text);
+                toggleDialog(); // Close the dialog on success
+            }, (error) => {
+                console.log('Failed to send email.', error.text);
+            });
+    };
+
     return (
         <div>
             <button className={styles['feedback-button']} onClick={toggleDialog}>
-                Give Feedback
+                Feedback
             </button>
 
             {isOpen && (
                 <div className={styles['dialog-background']}>
-                    <div className="dialog">
+                    <div className={styles['dialog']}>
                         <button className={styles["close-button"]} onClick={toggleDialog}>×</button>
                         <h2>Feedback</h2>
-                        <form onSubmit={(e) => {
-                            e.preventDefault();
-                            // Form submission logic here
-                            toggleDialog();
-                        }}>
+                        <form onSubmit={sendEmail}>
                             <input name="name" type="text" placeholder="Your name" required />
+                            <input name="company" type="text" placeholder="Your company" />
                             <input name="email" type="email" placeholder="Your email" required />
-                            <textarea name="description" placeholder="Your feedback" rows="4" required></textarea>
+                            <textarea name="message" placeholder="Your feedback" rows="8" required></textarea>
                             <button type="submit">Submit</button>
                         </form>
                     </div>
@@ -32,4 +42,4 @@ export default function Feedback() {
             )}
         </div>
     );
-};
+}
