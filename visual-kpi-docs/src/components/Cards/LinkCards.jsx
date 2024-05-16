02a12cffa6cda8b0dc7408d2af073e2d947a8b01
Heitor José Tessaro <heitor@writechoice.com.br>
2024-05-16 10:08:36 -0300

    style: fix style linkCards

--- visual-kpi-docs/src/components/Cards/LinkCards.jsx
+++ visual-kpi-docs/src/components/Cards/LinkCards.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import CardList from './CardList';
 
-const LinkCards = ({ data, numColumns }) => {
-  return <CardList data={data} numColumns={numColumns} className={"link_cards_container"} />;
+const LinkCards = ({ data, numColumns, className }) => {
+  return <CardList data={data} numColumns={numColumns} className={`link_cards_container ${className}`} />;
 };
 
 export default LinkCards;
