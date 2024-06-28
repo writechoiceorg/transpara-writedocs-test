2640fe23fad066e1468cb3eaa96f59476581d17e
Heitor José Tessaro <heitor@writechoice.com.br>
2024-06-28 06:37:44 -0300

    feat: update SvgFont compo degnent
    now, you can use the rotate prop to rotate the icon 90 deg

--- visual-kpi-docs/src/components/SvgFont/SvgFont.jsx
+++ visual-kpi-docs/src/components/SvgFont/SvgFont.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import selection from '@site/static/font/selection.json';
 import positions from '@site/static/data/icons/iconsPosition.json';
 
-export default function SvgFont({ icon, size }) {
+export default function SvgFont({ icon, size, rotate = false }) {
   const [path, setPath] = useState('');
   const position = positions["100"];
   const data = selection.icons[parseInt(icon)];
@@ -10,19 +10,21 @@ export default function SvgFont({ icon, size }) {
   useEffect(() => {
     const path = data.icon.paths.reduce((acc, curr) => acc + ` ${curr}`, '');
     setPath(path);
-  }, []);
+  }, [data.icon.paths]);
+
+  const rotationStyle = rotate ? { transform: 'rotate(90deg)' } : {};
 
   return (
     <span className="svg_container">
-      <svg className="hidesvg" version="1.1" xmlns="http://www.w3.org/1999/xlink">
+      <svg className="hidesvg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
         <defs>
           <symbol id={data.properties.name} width={size} viewBox={`0 0 ${position}`}>
-            <title>test</title>
+            <title>{data.properties.name}</title>
             <path className="path1" d={path}></path>
           </symbol>
         </defs>
       </svg>
-      <svg className="icon"><use href={`#${data.properties.name}`}></use></svg>
+      <svg className="icon" style={rotationStyle}><use href={`#${data.properties.name}`}></use></svg>
     </span>  
-  )
+  );
 }
