ed0a8d7c2e5e21db5422e89cb272eb6d0641e763
Gabriel Gonçalves <gabrielraeder@outlook.com.br>
2024-07-02 12:06:40 -0300

    feat: new icon

--- visual-kpi-docs/src/components/SvgFont/SvgFont.jsx
+++ visual-kpi-docs/src/components/SvgFont/SvgFont.jsx
@@ -4,25 +4,32 @@ import positions from '@site/static/data/icons/iconsPosition.json';
 
 export default function SvgFont({ icon, size }) {
   const [path, setPath] = useState('');
-  const position = positions["100"];
+  console.log(selection.icons.length)
   const data = selection.icons[parseInt(icon)];
 
-  useEffect(() => {
-    const path = data.icon.paths.reduce((acc, curr) => acc + ` ${curr}`, '');
-    setPath(path);
-  }, []);
-
+  if (data.icon.paths) {
+    useEffect(() => {
+      const path = data.icon.paths.reduce((acc, curr) => acc + ` ${curr}`, '');
+      setPath(path);
+    }, []);
+  
+    return (
+      <span className="svg_container">
+        <svg className="hidesvg" version="1.1" xmlns="http://www.w3.org/1999/xlink">
+          <defs>
+            <symbol id={data.properties.name} width={size} viewBox="0 0 1024 1024">
+              <title>test</title>
+              <path className="path1" d={path}></path>
+            </symbol>
+          </defs>
+        </svg>
+        <svg className="icon"><use href={`#${data.properties.name}`}></use></svg>
+      </span>  
+    )
+  }
   return (
     <span className="svg_container">
-      <svg className="hidesvg" version="1.1" xmlns="http://www.w3.org/1999/xlink">
-        <defs>
-          <symbol id={data.properties.name} width={size} viewBox={`0 0 ${position}`}>
-            <title>test</title>
-            <path className="path1" d={path}></path>
-          </symbol>
-        </defs>
-      </svg>
-      <svg className="icon"><use href={`#${data.properties.name}`}></use></svg>
-    </span>  
+      <span className="icon" dangerouslySetInnerHTML={{ __html: data.icon }} />
+    </span>
   )
 }
