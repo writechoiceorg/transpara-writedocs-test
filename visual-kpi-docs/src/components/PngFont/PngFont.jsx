39beb01b2da20d26128daa89f4da3bd5adf4b390
Heitor José Tessaro <heitor@writechoice.com.br>
2024-08-07 16:14:47 -0300

    chore: removes consolelog from PngFont component.

--- visual-kpi-docs/src/components/PngFont/PngFont.jsx
+++ visual-kpi-docs/src/components/PngFont/PngFont.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function PngFont({name}) {
-  console.log(name);
   return (
     <span>
       <img src={useBaseUrl(`img/icons/designer/${name}.png`)} alt={`${name} icon`} style={{ display: 'inline', height: '18px', width: '18px' }} />
