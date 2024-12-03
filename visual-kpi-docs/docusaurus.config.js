27c5fe4308eb7a751bf2b693fe679ef06b77205b
Heitor José Tessaro <85108199+htessaro@users.noreply.github.com>
2024-12-03 14:50:44 -0300

    Update docusaurus.config.js

--- visual-kpi-docs/docusaurus.config.js
+++ visual-kpi-docs/docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://transpara.docs.writedocs.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it's often '/<projectName>/'
   baseUrl: '/',
